90b8c26a7d9e1d159231fea6ea023ee83101ac47
Nathan Sharp <nath20an06@gmail.com>
2025-05-21 21:23:07 +0100

    Fixed spelling in dev warning

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Development Warning
 
 function Development_Warning() {
-    alert("Warning this content is in development, it behave in unexpected ways or provide incorrect information.");
+    alert("Warning this content is in development, it may behave in unexpected ways or provide incorrect information.");
 }
